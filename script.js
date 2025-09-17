404fd5976a11cafd59d5285410b66e53a054d3c0
AK4-Arfaj <ak4.arfaj@gmail.com>
2025-09-17 11:29:37 +0300

    Completed TODO-3, added a string object and logged on the console what characters are at some of its indicies and its length

--- script.js
+++ script.js
@@ -64,6 +64,11 @@ Task:
 1) Create a String object or plain string.
 2) Use .charAt(index) and .length to output characters and size.
 */
+const str = "Advanced";
+console.log(str.charAt(0));
+console.log(str.charAt(2));
+console.log(str.length);
+
 
 // ===================================
 // TODO-4: DATE — day, month, and year
